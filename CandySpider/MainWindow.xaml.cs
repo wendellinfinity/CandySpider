3fe121f536a006b9dc10568cc82ee11917d67ca3
wendellinfinity <wendell.hack@gmail.com>
2011-10-30 11:30:54 +0800

    Initial commit
    Kindly check the README file

---
+++ CandySpider/MainWindow.xaml.cs
@@ -0,0 +1,323 @@
+﻿/*   CandySpider.cs
+ *   Author: Wendell
+ *   Date Created: 10/27/2011
+ *   
+ *   Please email me at wendell.hack@gmail.com if you'd be adopting the code
+ *   
+ *   Thanks
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.IO.Ports;
+using System.Threading;
+using System.IO;
+
+namespace CandySpider {
+     /// <summary>
+     /// Interaction logic for MainWindow.xaml
+     /// </summary>
+     public partial class MainWindow : Window {
+
+          // XBEE explorer serial port
+          private SerialPort _port = new SerialPort("COM3", 9600, Parity.None, 8, StopBits.One);
+
+          DispatcherTimer _spiderTimer; // timer for the spider animation
+          DispatcherTimer _spiderHitTimer; // timer for the spider hit animation
+          DispatcherTimer _gameTimer; // main game timer
+          DispatcherTimer _timeIsUpTimer; // just some "timer to blink the time is up text"
+          int _animInterval = 5, // animation frame interval 
+               _hitAnimInterval = 500, // time when the spider's eyes are in "ouch" mode
+               _gameInterval = 1000; // 1 second ticks
+          int _maxTicks = 80, // ticks for the spider animation
+               _maxGameTicks = 30, // max game time
+               _gameTicks = 30, // how many seconds
+               _maxSpiders = 100; // cap for max spiders hit
+          double _spiderStep = 0; // spider animation translate count
+          bool _goingDown = true, // is spider going down 
+               _gameStarted = false; // is game started
+          Image spider; // spider animation
+          Uri spiderURI, spiderOOURI; // location for spider sprites resource
+          BitmapImage spiderImage, spiderOOImage; // loaded spider sprites
+          Thread _listenerThread; // thread to listen for XBEE serial inputs
+          bool timeIsUpAnimationFlag=false; // is time is up
+
+          bool _isHit = false; // tells if a spider was hit (from serial input or button)
+          int _spidersCaught = 0; // counts spiders
+
+
+          public MainWindow() {
+               InitializeComponent();
+               // get the resources and load the bitmaps for the spider animation
+               spiderURI = new Uri(@"/CandySpider;component/spider.png", UriKind.Relative);
+               spiderImage = new BitmapImage(spiderURI);
+               spiderOOURI = new Uri(@"/CandySpider;component/spiderOO.png", UriKind.Relative);
+               spiderOOImage = new BitmapImage(spiderOOURI);
+
+               // initialize the spider animation
+               this.spider = new Image();
+               // lay out in canvas first but initialized in code instead because of sprites
+               //<Image Height="417" HorizontalAlignment="Left" Name="spider" Stretch="Fill" VerticalAlignment="Top" Width="533" />
+               this.spider.Height = 417;
+               this.spider.HorizontalAlignment = HorizontalAlignment.Left;
+               this.spider.Name = "spider";
+               this.spider.Stretch = Stretch.Fill;
+               this.spider.VerticalAlignment = VerticalAlignment.Top;
+               this.spider.Width = 533;
+               this.spider.Source = spiderOOImage;
+               this.drawArea.Children.Add(this.spider); // add to canvas so it will be easier to translate (Canvas.Set/Get)
+
+               // initialize the spider anumation timer
+               this._spiderTimer = new DispatcherTimer();
+               this._spiderTimer.Interval = new TimeSpan(0, 0, 0, 0, this._animInterval);
+               // perform the necessary actions per tick
+               this._spiderTimer.Tick += new EventHandler(delegate(object sender, EventArgs e) {
+                    this.AnimateSpider(); // animate spider!
+               });
+               this._spiderTimer.Start(); // start immediately
+
+               // this is just for the "ouch" animation of the spider; dont start yet
+               this._spiderHitTimer = new DispatcherTimer();
+               this._spiderHitTimer.Interval = new TimeSpan(0, 0, 0, 0, this._hitAnimInterval);
+               this._spiderHitTimer.Tick += new EventHandler(delegate(object sender, EventArgs e) {
+                    this.ResetSpider();
+               });
+
+               // initialize the main game timer but dont start yet
+               this._gameTimer = new DispatcherTimer();
+               this._gameTimer.Interval = new TimeSpan(0, 0, 0, 0, this._gameInterval);
+               this._gameTimer.Tick += new EventHandler(delegate(object sender, EventArgs e) {
+                    this.GameCounter(); // starts game counter
+               });
+
+               // initialize the time is up text to blink always
+               // initially the text is hidden
+               this._timeIsUpTimer = new DispatcherTimer();
+               this._timeIsUpTimer.Interval = new TimeSpan(0, 0, 0, 0, 500);
+               this._timeIsUpTimer.Tick += new EventHandler(delegate(object sender, EventArgs e) {
+                    // just switch between 2 colors
+                    if (timeIsUpAnimationFlag) {
+                         timeIsUp.Foreground = Brushes.Yellow;
+                         timeIsUpAnimationFlag = false;
+                    }
+                    else {
+                         timeIsUp.Foreground = Brushes.Red;
+                         timeIsUpAnimationFlag = true;
+                    }
+               });
+               this._timeIsUpTimer.Start(); // start immediately
+
+               // initialize the game
+               ResetGame();
+               
+               // start the listener thread for the XBEE input
+               _listenerThread = new Thread(new ParameterizedThreadStart(delegate(object o) {
+                    ListenPort();
+               }));
+               _listenerThread.Start(); // start
+          }
+
+          /// <summary>
+          /// Method that constantly listens to the serial input stream
+          /// </summary>
+          private void ListenPort() {
+               // initialize the sensor port, mine was registered as COM3, you may check yours
+               // through the hardware devices from control panel
+               int bytesToRead = 0;
+               string chunk, message;
+               _port.Open();
+               try {
+                    bool start;
+                    start = false;
+                    message = "";
+                    while (true) {
+                         // check if there are bytes incoming
+                         bytesToRead = _port.BytesToRead;
+                         if (bytesToRead > 0) {
+                              byte[] input = new byte[bytesToRead];
+                              // read the Xbee's input
+                              _port.Read(input, 0, bytesToRead);
+                              // convert the bytes into string
+                              chunk = System.Text.Encoding.UTF8.GetString(input);
+                              if (chunk.IndexOf("[") >= 0) {
+                                   start = true;
+                              }
+                              if (start) {
+                                   message += chunk;
+                              }
+                              if (chunk.IndexOf("]") >= 0) {
+                                   start = false;
+                                   // clean up code
+                                   message = message.Trim().Replace("[", "").Replace("]", "");
+                                   // the message expects "[hit]" but just removed the brackets
+                                   if ("hit".Equals(message)) {
+                                        Dispatcher.BeginInvoke(DispatcherPriority.Input, new ThreadStart(() =>
+                                        {
+                                             HitSpider();
+                                        }));
+                                   }
+                                   message = "";
+                              }
+                              Console.WriteLine("");
+                         }
+                    }
+
+               }
+               finally {
+                    // again always close the serial ports!
+                    _port.Close();
+               }
+          }
+
+          /// <summary>
+          /// Main game counter method, it updates the time display and starts/stop the game
+          /// </summary>
+          private void GameCounter() {
+               if (_gameStarted) {
+                    if (_gameTicks > 0) {
+                         _gameTicks--; // count down
+                    }
+                    time.Content = string.Format("Time: {0}s", _gameTicks.ToString("00"));
+                    if (_gameTicks == 0) {
+                         _gameStarted = false; // stop the game
+                         _gameTimer.Stop(); // stop the game timer
+                         timeIsUp.Visibility = System.Windows.Visibility.Visible; // show the "time is up" text 
+                         //ResetGame();
+                    }
+               }
+          }
+
+          /// <summary>
+          /// Animates the spider to move up and down
+          /// </summary>
+          protected void AnimateSpider() {
+               TranslateTransform move;
+               if (_goingDown) {
+                    // translate down
+                    move = new TranslateTransform(0, this._spiderStep);
+               }
+               else {
+                    // translate up
+                    move = new TranslateTransform(0, this._spiderStep);
+               }
+               // check if moving down or up
+               if (_goingDown) {
+                    _spiderStep++;
+               }
+               else {
+                    _spiderStep--;
+               }
+               // then set if to go down or up next
+               if (_spiderStep > _maxTicks) {
+                    _goingDown = false;
+               }
+               if (_spiderStep == 0) {
+                    _goingDown = true;
+               }
+               // apply the movement
+               this.spider.RenderTransform = move;
+          }
+
+          /// <summary>
+          /// Resets the spider's expression to round eyes
+          /// </summary>
+          private void ResetSpider() {
+               if (!_isHit) {
+                    this.spider.Source = spiderOOImage;
+                    this._spiderHitTimer.Stop(); 
+                    return;
+               }
+               _isHit = false;
+          }
+
+          /// <summary>
+          /// Changes the spider's expression to ouch and also updates the number of spiders hit
+          /// </summary>
+          protected void HitSpider() {
+               if (_gameStarted) {
+                    if (!_isHit) {
+                         _isHit = true;
+                         this.spider.Source = spiderImage; // ouch eyes
+                         this._spiderHitTimer.Start();
+                    }
+                    // cap for max spiders, also increments
+                    if (++_spidersCaught >= _maxSpiders) {
+                         return;
+                    }
+                    // update the display counter
+                    hitCount.Content = string.Format("{0}", _spidersCaught.ToString("00"));
+               }
+          }
+
+          /// <summary>
+          /// Resets the game
+          /// </summary>
+          private void ResetGame() {
+               // the button does not disappear anyway
+               start.Visibility = System.Windows.Visibility.Visible;
+               _gameStarted = false; // signal game start
+               // reset counters
+               _spidersCaught = 0; 
+               hitCount.Content = string.Format("00");
+               _gameTicks = _maxGameTicks;
+               time.Content = string.Format("Time: {0}s", _gameTicks.ToString("00"));
+               // reset game timer
+               _gameTimer.Stop();
+               timeIsUp.Visibility = System.Windows.Visibility.Hidden;
+          }
+
+          /// <summary>
+          /// Starts the game!
+          /// </summary>
+          private void StartGame() {
+               if (!_gameStarted) {
+                    // button does not hide anyway
+                    start.Visibility = System.Windows.Visibility.Hidden;
+                    ResetGame(); // reset the game
+                    _gameStarted = true;
+                    _gameTimer.Start(); // start the game timer
+               }
+          }
+
+          /// <summary>
+          /// Some button to simulate the spider hit
+          /// </summary>
+          /// <param name="sender"></param>
+          /// <param name="e"></param>
+          private void rawr_Click(object sender, RoutedEventArgs e) {
+               HitSpider();
+          }
+
+          /// <summary>
+          /// Resets the game
+          /// </summary>
+          /// <param name="sender"></param>
+          /// <param name="e"></param>
+          private void reset_Click(object sender, RoutedEventArgs e) {
+               ResetGame();
+          }
+
+
+          /// <summary>
+          /// Starts a game
+          /// </summary>
+          /// <param name="sender"></param>
+          /// <param name="e"></param>
+          private void start_Click(object sender, RoutedEventArgs e) {
+               StartGame();
+          }
+
+     }
+}
---
+++ CandySpiderController/Program.cs
@@ -0,0 +1,170 @@
+﻿/*   Program.cs
+ *   Author: Wendell
+ *   Date Created: 10/27/2011
+ *   
+ *   Please email me at wendell.hack@gmail.com if you'd be adopting the code
+ *   
+ *   Thanks
+ */
+
+using System;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+using System.IO.Ports;
+
+namespace CandySpiderController {
+     public class Program {
+
+          // serial port to be user be XBEE to send data
+          static SerialPort _serialPort = new SerialPort("COM1", 9600, Parity.None, 8, StopBits.One);
+          static OutputPort[] _spiders; // LED ports, used the analog marked ports for these
+          //static OutputPort _hitIndicator;
+          static InterruptPort[] _hits; // the switch input ports, used digital marked ports on netduino
+          static Random _random; // random generator
+          static bool[] _states; // keeps states for each LED
+          static Thread[] _runners; // runners for each LED to flash randomly
+          public static void Main() {
+               //_hitIndicator = new OutputPort(Cpu.Pin.GPIO_Pin13, false);
+               _random = new Random(); // initialize random
+               // initialize the LED ports
+               _spiders = new OutputPort[6] { 
+                    new OutputPort(Pins.GPIO_PIN_A0, false),
+                    new OutputPort(Pins.GPIO_PIN_A1, false),
+                    new OutputPort(Pins.GPIO_PIN_A2, false),
+                    new OutputPort(Pins.GPIO_PIN_A3, false),
+                    new OutputPort(Pins.GPIO_PIN_A4, false),
+                    new OutputPort(Pins.GPIO_PIN_A5, false)
+               };
+               // initialize LED port states
+               _states = new bool[6] { false, false, false, false, false, false };
+               // initialize the Interrupt ports for the input, only detect when pressed on (signal to HIGH)
+               _hits = new InterruptPort[6] { 
+                    new InterruptPort(Pins.GPIO_PIN_D3,true,ResistorModes.Disabled,InterruptModes.InterruptEdgeLevelHigh),
+                    new InterruptPort(Pins.GPIO_PIN_D4,true,ResistorModes.Disabled,InterruptModes.InterruptEdgeLevelHigh),
+                    new InterruptPort(Pins.GPIO_PIN_D5,true,ResistorModes.Disabled,InterruptModes.InterruptEdgeLevelHigh),
+                    new InterruptPort(Pins.GPIO_PIN_D6,true,ResistorModes.Disabled,InterruptModes.InterruptEdgeLevelHigh),
+                    new InterruptPort(Pins.GPIO_PIN_D7,true,ResistorModes.Disabled,InterruptModes.InterruptEdgeLevelHigh),
+                    new InterruptPort(Pins.GPIO_PIN_D8,true,ResistorModes.Disabled,InterruptModes.InterruptEdgeLevelHigh)
+               };
+               // open the serialport
+               _serialPort.Open();
+               // loop through the input ports
+               for (int i = 0; i < 6; i++) {
+                    // assign interrupt event delegate on each of the input ports
+                    _hits[i].OnInterrupt += new NativeEventHandler(
+                         delegate(uint data1, uint data2, DateTime time) {
+                              Thread runner = null; // just to contain the current thread
+                              bool hasHit = false;
+                              OutputPort spider = null; // just to contain the matching LED for the switch
+                              for (int p = 0; p < 6; p++) {
+                                   // get the matching pin from the interrupt
+                                   if ((uint)_hits[p].Id == data1) {
+                                        _hits[p].ClearInterrupt(); // clear the interrupt
+                                        // then get the associated thread for the LED and the Outputport
+                                        switch (data1) {
+                                             case (uint)Pins.GPIO_PIN_D3:
+                                                  runner = _runners[5];
+                                                  spider = _spiders[5];
+                                                  break;
+                                             case (uint)Pins.GPIO_PIN_D4:
+                                                  runner = _runners[4];
+                                                  spider = _spiders[4];
+                                                  break;
+                                             case (uint)Pins.GPIO_PIN_D5:
+                                                  runner = _runners[3];
+                                                  spider = _spiders[3];
+                                                  break;
+                                             case (uint)Pins.GPIO_PIN_D6:
+                                                  runner = _runners[2];
+                                                  spider = _spiders[2];
+                                                  break;
+                                             case (uint)Pins.GPIO_PIN_D7:
+                                                  runner = _runners[1];
+                                                  spider = _spiders[1];
+                                                  break;
+                                             case (uint)Pins.GPIO_PIN_D8:
+                                                  runner = _runners[0];
+                                                  spider = _spiders[0];
+                                                  break;
+                                             default:
+                                                  break;
+                                        }
+                                   }
+                              }
+                              // if the output port is HIGH (lighted up)
+                              // and this interrupt says the switch is hit...
+                              if (spider.Read()) {
+                                   // just flag (simpler too if I just moved the next if block below here
+                                   hasHit = true;
+                              }
+                              // check the flag, i can just move this code above, but i was thinking of doing something...
+                              if (hasHit) {
+                                   // suspend the current thread that runs the LED light
+                                   runner.Suspend();
+                                   spider.Write(false); // set the LED to LOW (off)
+                                   SendSerial("[hit]"); // send serial through XBEE
+                                   Debug.Print(data1.ToString() + " : " + data2.ToString());
+                              }
+                         });
+               }
+               // initialize the threads for the randomly lighting LEDs
+               _runners = new Thread[6];
+               for (int t = 0; t < 6; t++) {
+                    OutputPort spider = _spiders[t]; // just the current LED in the loop for use of delegate
+                    _runners[t] = new Thread(delegate() {
+                         int enable, duration;
+                         // loop endlessly (but not)
+                         while (true) {
+                              // get if the LED will light up
+                              enable = _random.Next(2);
+                              if (enable > 0) {
+                                   spider.Write(true); // on
+                              }
+                              else {
+                                   spider.Write(false); // off
+                              }
+                              // randomize how long it will light up
+                              duration = _random.Next(5);
+                              // the max will be 1.5 seconds but, fastest is 500ms
+                              System.Threading.Thread.Sleep((duration*200)+500);
+                              // then turn off the LED after (this is an opportunity for the interrupt to happen)
+                              spider.Write(false); 
+                              System.Threading.Thread.Sleep(800); // sleep the thread
+                              System.Threading.Thread.CurrentThread.Suspend();
+                         }
+                    });
+                    _runners[t].Start(); // start to light them up!
+               }
+               // this loop just wakes up all of the sleeping LEDs so that they will light up again
+               // when an interrupt took it off or the LED was not hit but turned off
+               while (true) {
+                    for (int t = 0; t < 6; t++) {
+                         if (_runners[t].ThreadState != ThreadState.Running) {
+                              _runners[t].Resume();
+                         }
+                    }
+               }
+          }
+
+          /// <summary>
+          /// Sends message to serial port
+          /// </summary>
+          /// <param name="message"></param>
+          private static void SendSerial(string message) {
+               int bytesToRead = _serialPort.BytesToRead;
+               byte[] buffer = new byte[message.Length];
+               if (bytesToRead > 0) {
+                    // get the waiting data
+                    byte[] bufferIn = new byte[bytesToRead];
+                    // READ any data received
+                    _serialPort.Read(bufferIn, 0, bufferIn.Length);
+               }
+               buffer = System.Text.Encoding.UTF8.GetBytes(message);
+               _serialPort.Write(buffer, 0, buffer.Length);
+          }
+
+     }
+}
